bcfa0c7c2b8dd6eec610a49f2030397ec3174436
dhruv8056 <dhruvmangukiya7841@gmail.com>
2026-03-25 17:40:38 +0530

    scroll issue fixed

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import 'swiper/css/scrollbar';
 import 'swiper/swiper-bundle.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ConfirmDialogProvider } from '@shared/components/confirmModal/view/confirmDialogProvider';
+import ScrollToTop from '@shared/components/common/ScrollToTop';
 
 function App() {
   return (
     <>
       <Router>
         <ConfirmDialogProvider>
+          <ScrollToTop />
           <AppRoutes />
         </ConfirmDialogProvider>
       </Router>
